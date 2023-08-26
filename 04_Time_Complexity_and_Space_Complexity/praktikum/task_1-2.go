50a9fa5865cc10e84be5466243eae5caf3d2305f
aimrzki <muhammadaimar77@gmail.com>
2023-08-27 00:34:16 +0700

    Pengumpulan tugas 10 Clean Code

--- 04_Time_Complexity_and_Space_Complexity/praktikum/task_1-2.go
+++ 04_Time_Complexity_and_Space_Complexity/praktikum/task_1-2.go
@@ -5,14 +5,20 @@ import (
 )
 
 func pow(x, n int) int {
+	// kondisi pertama: jika n adalah 0, maka langsung mengembalikan nilai 1.
 	if n == 0 {
 		return 1
 	}
+	// kondisi kedua: jika n genap, maka menghitung nilai pangkat
 	if n%2 == 0 {
+		// Menghitung nilai pangkat dengan memanggil fungsi pow
 		temp := pow(x, n/2)
+		// Mengembalikan hasil perhitungan pangkat
 		return temp * temp
 	}
+	// kondisi ketiga: jika n ganjil, maka menghitung nilai pangkat
 	temp := pow(x, (n-1)/2)
+	// Mengembalikan hasil perhitungan pangkat .
 	return x * temp * temp
 }
 
