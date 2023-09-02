87669726f7e35ec2e629e30099bc7d8059365762
Aimar Rizki <96635816+aimrzki@users.noreply.github.com>
2023-09-03 03:09:38 +0700

    Revisi tugas 12

--- 12_Join_Union_Agregasi_Subquery_Function/praktikum/Tugas12_DBMS.sql
+++ 12_Join_Union_Agregasi_Subquery_Function/praktikum/Tugas12_DBMS.sql
@@ -1,160 +1,177 @@
-# Buat table product_types
-CREATE TABLE product_type (
-    id INT(11) PRIMARY KEY not null auto_increment,
-    name VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+create table operators(
+    id int(11) not null auto_increment,
+    name varchar(255),
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default  current_timestamp,
+    primary key (id)
 );
 
-# Buat tabel operator
-CREATE TABLE operators (
-    id INT(11) PRIMARY KEY not null auto_increment,
-    name VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+create table product_types(
+    id int(11) not null auto_increment,
+    name varchar(255),
+     created_at timestamp default current_timestamp,
+    updated_at timestamp default  current_timestamp,
+    primary key (id)
 );
 
-# Buat tabel products
-CREATE TABLE products (
-    id INT(11) PRIMARY KEY not null auto_increment,
-    product_type_id INT(11),
-    operator_id INT(11),
-    code VARCHAR(50),
-    name VARCHAR(100),
-    status SMALLINT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (product_type_id) REFERENCES product_type(id),
-    FOREIGN KEY (operator_id) REFERENCES operators(id)
+create table products(
+    id int(11) not null auto_increment,
+    product_type_id int(11),
+    operator_id int(11),
+    code varchar(50),
+    name varchar(100),
+    status smallint,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default  current_timestamp,
+    primary key (id),
+    foreign key (product_type_id) references product_types(id),
+    foreign key (operator_id) references  operators(id)
 );
 
-# Buat table product_description
-CREATE TABLE product_description (
-    id INT(11) PRIMARY KEY not null auto_increment,
-    description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+create table product_description (
+    id int (11) not null auto_increment ,
+    description text,
+     created_at timestamp default current_timestamp,
+    updated_at timestamp default  current_timestamp,
+    primary key(id)
 );
 
-# Buat tabel payment_method
-CREATE TABLE payment_methods (
-    id INT(11) PRIMARY KEY not null auto_increment,
-    name VARCHAR(255),
-    status SMALLINT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+create table payment_methods(
+    id int(11) not null auto_increment,
+    name varchar(255),
+    status smallint,
+     created_at timestamp default current_timestamp,
+    updated_at timestamp default  current_timestamp,
+    primary key (id)
 );
 
-# Buat table users
-CREATE TABLE users (
-    id INT(11) PRIMARY KEY not null auto_increment,
-    name VARCHAR(255),
-    status SMALLINT,
-    dob DATE,
-    gender CHAR(1),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+create table users(
+    id int(11) not null auto_increment,
+    status smallint,
+    dob date,
+    gender char(1),
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default  current_timestamp,
+    primary key (id)
 );
 
-# Buat tabel transaction
-CREATE TABLE transactions (
-    id INT(11) PRIMARY KEY not null auto_increment,
-    user_id INT(11),
-    payment_method_id INT(11),
-    status VARCHAR(10),
-    total_qty INT(11),
-    total_price NUMERIC(25, 2),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (payment_method_id) REFERENCES payment_methods(id)
+create table transactions (
+    id int(11) not null auto_increment,
+    user_id int(11),
+    payment_method_id int(11),
+    status varchar(10),
+    total_qty int(11),
+    total_price numeric(25,2),
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default  current_timestamp,
+    primary key (id),
+    foreign key (user_id) references users(id),
+    foreign key (payment_method_id) references payment_methods(id)
 );
 
-# Buat table transaction_details
-CREATE TABLE transaction_details (
-    id INT(11) not null auto_increment PRIMARY KEY,
-    transaction_id INT(11),
-    product_id INT(11),
-    status VARCHAR(10),
-    qty INT(11),
-    price NUMERIC(25, 2),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (transaction_id) REFERENCES transactions(id),
-    FOREIGN KEY (product_id) REFERENCES products(id)
+create table transactions_detail(
+    transaction_id int(11) not null,
+    product_id int(11),
+    status varchar(10),
+    qty int(11),
+    price numeric(25,2),
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default  current_timestamp,
+    primary key (transaction_id,product_id),
+    foreign key (transaction_id) references transactions(id),
+    foreign key (product_id) references products(id)
 );
 
 # 1A
-INSERT INTO operators (name)
-VALUES
-    ('P0001'),
-    ('P0002'),
-    ('P0003'),
-    ('P0004'),
-    ('P0005');
 
-select * from operators;
+insert into operators (name)
+values ('operator A'),
+       ('operator B'),
+       ('operator C'),
+       ('operator D'),
+       ('operator E');
 
 # 1B
-INSERT INTO product_type (name)
-VALUES
-    ('Model S'),
-    ('Model 3'),
-    ('Model Y');
 
-select * from product_type;
+insert into product_types (name)
+values ('Sedan'),
+       ('SUV'),
+       ('Truck');
 
 # 1C
-INSERT INTO products (product_type_id, operator_id, code, name, status)
-VALUES
-    (1, 3, 'P001', 'Tesla Model S', 1),
-    (1, 3, 'P002', 'Tesla Model S P90D ', 1);
-
-select * from products;
+insert into products (product_type_id, operator_id, code, name, status)
+values (1,3,'T0001','Tesla Model S P50D',1),
+       (1,3,'T0001','Tesla Model S P90D',1);
 
 # 1D
-INSERT INTO products (product_type_id, operator_id, code, name, status)
-VALUES
-    (2, 1, 'P003', 'Tesla Model 3 P50D', 1),
-    (2, 1, 'P004', 'Tesla Model 3 P60D', 1),
-    (2, 1, 'P005', 'Tesla Model 3 P90D', 1);
-
-select * from products;
+insert into products (product_type_id, operator_id, code, name, status)
+values (2,1,'T0002','Tesla Model X P50D',1),
+       (2,1,'T0003','Tesla Model X P90D',1);
 
 # 1E
-INSERT INTO products (product_type_id, operator_id, code, name, status)
-VALUES
-    (3, 4, 'P006', 'Tesla Model Y P50D', 1),
-    (3, 4, 'P007', 'Tesla Model Y P60D', 1),
-    (3, 4, 'P008', 'Tesla Model Y P90D', 1);
-
-select * from products;
+insert into products (product_type_id, operator_id, code, name, status)
+values (3,4,'T0004','Tesla Cybertruck White',0),
+       (3,4,'T0005','Tesla Cybertruck Black',0);
 
 # 1F
-INSERT INTO product_description (description)
-VALUES
-    ('Mobil Tesla Model S Standard with range 450 KM'),
-    ('Mobil Tesla Model S Standard with range 900 KM'),
-    ('Mobil Tesla Model 3 Standard with range 500 KM'),
-    ('Mobil Tesla Model 3 Standard with range 600 KM'),
-    ('Mobil Tesla Model 3 Standard with range 900 KM'),
-    ('Mobil Tesla Model Y Standard with range 500 KM'),
-    ('Mobil Tesla Model Y Standard with range 600 KM'),
-    ('Mobil Tesla Model Y Standard with range 900 KM');
+insert into product_description (description)
+values ('Tesla model S with range 500km'),
+       ('Tesla model S with range 900km'),
+       ('Tesla model X with range 500km'),
+       ('Tesla model X with range 900km'),
+       ('Tesla Cybertruck with white color'),
+       ('Tesla Cybertruck with black color');
 
 select * from product_description;
 
+alter table products
+add column  descriptions int(11);
+
+alter table products
+add constraint fk_product_description
+foreign key (descriptions) references product_description(id);
+
+update products
+set descriptions = 1
+where id =1;
+
+update products
+set descriptions = 2
+where id =2;
+
+update products
+set descriptions = 3
+where id =3;
+
+update products
+set descriptions = 4
+where id =4;
+
+update products
+set descriptions = 5
+where id =5;
+
+update products
+set descriptions = 6
+where id =6;
+
+select * from products
+join product_description on (products.descriptions = product_description.id);
 
 # 1G
-INSERT INTO payment_methods (name, status)
-VALUES
-    ('Kartu Debit/Kredit', 1),
-    ('Virtual Account', 1),
-    ('E-Wallet', 1);
 
-select * from products;
+insert into payment_methods (name, status)
+values ('Transfer Bank',1),
+       ('Virtual Account',1),
+       ('Kartu Debit / Kredit',1),
+       ('E-wallet',1),
+       ('COD',1);
 
 # 1H
+
+alter table users
+add column name varchar(255) not null ;
+
 INSERT INTO users (status, name,dob, gender)
 VALUES
     (1,'Aimar Rizki','2001-05-10', 'M'),
@@ -163,67 +180,113 @@ VALUES
     (1,'Ami','1998-08-30', 'F'),
     (1,'Yangti','2005-04-05', 'F');
 
-select * from users;
+# 1I
 
-# 1i
-INSERT INTO transactions (user_id, payment_method_id, status, total_qty, total_price)
-VALUES
-    (1, 1, 'Complete', 2, 1000000),
-    (1, 2, 'Pending', 1, 2500000),
-    (1, 3, 'Complete', 3, 3450000);
-
-INSERT INTO transactions (user_id, payment_method_id, status, total_qty, total_price)
-VALUES
-    (2, 1, 'Complete', 1, 450000),
-    (2, 2, 'Complete', 2, 750000),
-    (2, 3, 'Pending', 1, 325000);
-
-INSERT INTO transactions (user_id, payment_method_id, status, total_qty, total_price)
-VALUES
-    (3, 1, 'Pending', 2, 2350000),
-    (3, 2, 'Complete', 1, 5500000),
-    (3, 3, 'Complete', 3, 425000);
-
-select * from transactions;
-select * from transaction_details;
--- Modify the transaction_details table
--- Modify the transaction_details table
+insert into transactions (user_id, payment_method_id, status, total_qty, total_price)
+values (1,3,'success',1,100000),
+       (1,2,'success',1,150000),
+       (1,1,'success',1,200000),
+       (2,2,'success',1,300000),
+       (2,3,'success',1,450000),
+       (2,4,'success',1,500000),
+       (3,5,'success',1,200000),
+       (3,5,'success',1,350000),
+       (3,5,'success',1,400000),
+       (4,2,'success',1,200000),
+       (4,1,'success',1,150000),
+       (4,1,'success',1,200000),
+       (5,3,'success',1,110000),
+       (5,5,'success',1,150000),
+       (5,4,'success',1,200000);
 
 # 1J
 
-INSERT INTO transaction_details (transaction_id, product_id, status, qty, price)
-VALUES
-    (1, 1, 'Complete', 1, 1000000),
-    (1, 3, 'Complete', 1, 2500000),
-    (1,5, 'Complete', 1, 3450000);
+insert into transactions_detail (transaction_id, product_id, status, qty, price)
+values (1,1,'sucess',2,1000000),
+       (1,3,'sucess',1,3000000),
+       (1,4,'sucess',3,4000000);
 
-select * from transaction_details;
+insert into transactions_detail (transaction_id, product_id, status, qty, price)
+values (2,2,'sucess',2,1000000),
+       (2,3,'sucess',1,3000000),
+       (2,5,'sucess',3,4000000);
 
-select * from transactions;
+insert into transactions_detail (transaction_id, product_id, status, qty, price)
+values (3,1,'sucess',2,1000000),
+       (3,3,'sucess',1,3000000),
+       (3,4,'sucess',3,4000000);
 
-INSERT INTO transaction_details (transaction_id, product_id, status, qty, price)
-VALUES
-    (2, 2, 'Pending', 1, 450000),
-    (2, 4, 'Pending', 1, 75000),
-    (2, 6, 'Pending', 1, 325000);
+insert into transactions_detail (transaction_id, product_id, status, qty, price)
+values (4,1,'sucess',2,1000000),
+       (4,3,'sucess',1,3000000),
+       (4,4,'sucess',3,4000000);
 
-INSERT INTO transaction_details (transaction_id, product_id, status, qty, price)
-VALUES
-    (3, 1, 'Complete', 1, 2350000),
-    (3, 4, 'Complete', 2, 5500000),
-    (3, 7, 'Complete', 1, 425000);
+insert into transactions_detail (transaction_id, product_id, status, qty, price)
+values (5,1,'sucess',2,1000000),
+       (5,3,'sucess',1,3000000),
+       (5,4,'sucess',3,4000000);
 
-select * from transaction_details;
+insert into transactions_detail (transaction_id, product_id, status, qty, price)
+values (6,1,'sucess',2,1000000),
+       (6,3,'sucess',1,3000000),
+       (6,4,'sucess',3,4000000);
+
+insert into transactions_detail (transaction_id, product_id, status, qty, price)
+values (7,1,'sucess',2,1000000),
+       (7,3,'sucess',1,3000000),
+       (7,4,'sucess',3,4000000);
+
+insert into transactions_detail (transaction_id, product_id, status, qty, price)
+values (8,1,'sucess',2,1000000),
+       (8,3,'sucess',1,3000000),
+       (8,4,'sucess',3,4000000);
+
+insert into transactions_detail (transaction_id, product_id, status, qty, price)
+values (9,1,'sucess',2,1000000),
+       (9,3,'sucess',1,3000000),
+       (9,4,'sucess',3,4000000);
+
+insert into transactions_detail (transaction_id, product_id, status, qty, price)
+values (10,1,'sucess',2,1000000),
+       (10,3,'sucess',1,3000000),
+       (10,4,'sucess',3,4000000);
+
+insert into transactions_detail (transaction_id, product_id, status, qty, price)
+values (11,1,'sucess',2,1000000),
+       (11,3,'sucess',1,3000000),
+       (11,4,'sucess',3,4000000);
+
+insert into transactions_detail (transaction_id, product_id, status, qty, price)
+values (12,1,'sucess',2,1000000),
+       (12,3,'sucess',1,3000000),
+       (12,4,'sucess',3,4000000);
+
+insert into transactions_detail (transaction_id, product_id, status, qty, price)
+values (13,1,'sucess',2,1000000),
+       (13,3,'sucess',1,3000000),
+       (13,4,'sucess',3,4000000);
+
+insert into transactions_detail (transaction_id, product_id, status, qty, price)
+values (14,1,'sucess',2,1000000),
+       (14,3,'sucess',1,3000000),
+       (14,4,'sucess',3,4000000);
+
+insert into transactions_detail (transaction_id, product_id, status, qty, price)
+values (15,1,'sucess',2,1000000),
+       (15,3,'sucess',1,3000000),
+       (15,4,'sucess',3,4000000);
+
+select * from transactions_detail;
+
+# Select
 
 # 2A
-SELECT id, name
-FROM users
-WHERE gender = 'M';
+select name from users
+where gender='M';
 
 # 2B
-SELECT *
-FROM products
-WHERE id = 3;
+select * from products
+where id=3;
 
 # 2C
 SELECT *
@@ -242,36 +305,33 @@ SELECT *
 FROM users
 ORDER BY name ASC;
 
-# 2f
-SELECT *
-FROM products
-LIMIT 5;
+# 2F
+select * from products
+limit 5;
+
+# Update
 
 # 3A
 UPDATE products
-SET name = 'product dummy'
-WHERE id = 1;
-
-select * from products;
+set name = 'product dummy'
+where id = 1;
 
 # 3B
-UPDATE transaction_details
-SET qty = 3
-WHERE product_id = 1;
-
-select * from transaction_details;
+update transactions_detail
+set qty=3
+where product_id=1;
 
 # 4A
 DELETE FROM products
 WHERE id = 1;
 
-# 4A
+# 4B
 DELETE FROM products
 WHERE product_type_id = 1;
 
-# Join, Union, Sub, Query, Function
+# Join Union Subquery Function
 
-# Jawaban NO.1
+# 1
 SELECT *
 FROM transactions
 WHERE user_id = 1
@@ -282,39 +342,41 @@ SELECT *
 FROM transactions
 WHERE user_id = 2;
 
-# Jawaban NO.2
+# 2
 SELECT user_id, SUM(total_price) AS total_harga
 FROM transactions
 WHERE user_id = 1;
 
-# Jawaban NO.3
-SELECT SUM(td.qty) AS total_qty, SUM(td.price) AS total_price
-FROM transactions t
-JOIN transaction_details td ON t.id = td.transaction_id
-JOIN products p ON td.product_id = p.id
-WHERE p.product_type_id = 2;
+# 3
+select sum(transactions_detail.qty),sum(transactions_detail.price)
+from transactions
+join transactions_detail on(transactions.id=transactions_detail.transaction_id)
+join products on (transactions_detail.product_id=products.id)
+where products.product_type_id = 2;
 
+#4
 
-# Jawaban No.4
-SELECT p.*, pt.name AS product_type_name
-FROM products p
-JOIN product_type pt ON p.product_type_id = pt.id;
+SELECT *,product_types.name
+FROM products
+JOIN product_types on (products.product_type_id=product_types.id);
 
 # Jawaban no 5
-SELECT t.*, p.name AS product_name, u.name AS user_name,product_id
-FROM transactions t
-JOIN users u ON t.user_id = u.id
-JOIN transaction_details td ON t.id = td.transaction_id
-JOIN products p ON td.product_id = p.id;
+
+SELECT transactions.*, products.name AS product_name, users.name AS user_name, transactions_detail.product_id
+FROM transactions
+JOIN users ON transactions.user_id = users.id
+JOIN transactions_detail ON transactions.id = transactions_detail.transaction_id
+JOIN products ON transactions_detail.product_id = products.id;
 
 # Jawaban no 6
+
 DELIMITER //
 
 CREATE TRIGGER after_transaction_delete
 AFTER DELETE ON transactions
 FOR EACH ROW
 BEGIN
-    DELETE FROM transaction_details
+    DELETE FROM transactions_detail
     WHERE transaction_id = OLD.id;
 END;
 //
@@ -325,7 +387,7 @@ DELIMITER ;
 DELIMITER //
 
 CREATE TRIGGER after_transaction_detail_delete
-AFTER DELETE ON transaction_details
+AFTER DELETE ON transactions_detail
 FOR EACH ROW
 BEGIN
     DECLARE trans_id INT(11);
@@ -344,5 +406,5 @@ SELECT *
 FROM products
 WHERE id NOT IN (
     SELECT DISTINCT product_id
-    FROM transaction_details
+    FROM transactions_detail
 );
