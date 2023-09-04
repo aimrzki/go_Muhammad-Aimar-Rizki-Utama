792850e25e723e75f997fff28590fa8eb4173fd3
Aimar Rizki <96635816+aimrzki@users.noreply.github.com>
2023-09-04 22:14:32 +0700

    Penambahan Komentar untuk penjelasan

--- 08_ProblemSolvingParadigm_BruteForceGreedyandDynamicProgramming/praktikum/prioritas_1-4.go
+++ 08_ProblemSolvingParadigm_BruteForceGreedyandDynamicProgramming/praktikum/prioritas_1-4.go
@@ -7,11 +7,14 @@ import (
 func SimpleEquations(a, b, c int) {
 	for x := 1; x <= 10000; x++ {
 		for y := 1; y <= 10000; y++ {
+			// Hitung nilai z berdasarkan a, x, dan y.
 			z := a - x - y
 			if z <= 0 {
 				continue
 			}
 
+			// Jika x*y*z sama dengan b dan x^2 + y^2 + z^2 sama dengan c,
+			// maka ini adalah solusi yang benar.
 			if x*y*z == b && x*x+y*y+z*z == c {
 				fmt.Printf("%d %d %d\n", x, y, z)
 				return
